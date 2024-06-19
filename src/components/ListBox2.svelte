8523535dcbdc479408fc685015a4f6758d5df128
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-06-19 16:28:11 +0200

    prototype NoteSelector2.svelte that arranges note names horiz not vert

---
+++ src/components/ListBox2.svelte
@@ -0,0 +1,176 @@
+<script>
+  import { len } from "../util";
+  import "overlayscrollbars/overlayscrollbars.css";
+  import { OverlayScrollbars } from "overlayscrollbars";
+  import { getActiveTheme } from "../settings";
+
+  /** @type {{ 
+    items: any[],
+    onclick: (any) => void,
+    renderItem: any,
+    selectionChanged?: (any, number) => void,
+    initialSelection?: number,
+  }}*/
+  let {
+    items,
+    onclick,
+    renderItem,
+    selectionChanged = (el, idx) => {
+      /* no op */
+    },
+    initialSelection = 0,
+  } = $props();
+
+  let selectedIdx = $state(-1);
+
+  let n = len(items);
+  let refs = new Array(n);
+  let prevItemsLen = n;
+
+  // make sure to call selectionChanged() callback on initial
+  // selection, so if there's state calcualted based on that,
+  // it gets properly initalized
+  setTimeout(() => {
+    if (initialSelection > n - 1) {
+      initialSelection = n - 1;
+    }
+    if (n === 0) {
+      initialSelection = -1;
+    }
+    select(initialSelection);
+  }, 50);
+
+  $effect(() => {
+    let n = len(items);
+    if (n > len(refs)) {
+      console.log("expanding refs to:", n);
+      refs.length = n;
+    }
+
+    // TODO: this check shouldn't be necessary but
+    // this effect gets re-run after changing selection
+    if (n === prevItemsLen) {
+      return;
+    }
+    console.log("re-runinng effect:", len(items));
+    prevItemsLen = n;
+    // reset selection if changing items
+    if (n > 0) {
+      select(0);
+    } else {
+      select(-1);
+    }
+  });
+
+  /**
+   * @param {number} n
+   */
+  export function select(n) {
+    let nItems = len(items);
+    // console.log("select:", n, "nItems:", nItems);
+    if (nItems <= 0) {
+      if (selectedIdx != -1) {
+        selectedIdx = -1;
+        selectionChanged(null, -1);
+      }
+      return;
+    }
+    selectedIdx = 0;
+    if (n >= 0 && n < nItems) {
+      selectedIdx = n;
+    }
+    // console.log("selectedIdx:", selectedIdx);
+    let ref = refs[selectedIdx];
+    ref.scrollIntoView({ block: "nearest" });
+    let item = items[selectedIdx];
+    selectionChanged(item, selectedIdx);
+  }
+
+  export function selected() {
+    return items[selectedIdx];
+  }
+
+  export function up() {
+    let nItems = len(items);
+    if (nItems <= 0 || selectedIdx <= 0) {
+      return;
+    }
+    select(selectedIdx - 1);
+  }
+
+  export function down() {
+    let nItems = len(items);
+    console.log("donw: selectedIdx:", selectedIdx, "nItems:", nItems);
+    let lastIdx = nItems - 1;
+    if (nItems <= 0 || selectedIdx >= lastIdx) {
+      return;
+    }
+    select(selectedIdx + 1);
+  }
+
+  let listbox;
+  $effect(() => {
+    let theme = getActiveTheme();
+    // console.log("listbox overlay scrollbars, theme:", theme);
+    let opts = {};
+    if (theme === "dark") {
+      opts = {
+        scrollbars: {
+          theme: "os-theme-dark",
+        },
+      };
+    }
+    // OverlayScrollbars(listbox, opts);
+  });
+
+  /**
+   * @param {MouseEvent} ev
+   */
+  function listboxclick(ev) {
+    // note: could also traverse from ev.target via parentElement
+    // until finds tagName === "LI", but this seems more reliable
+    // slower but only executed on click
+    // could also put a data-index attribute on li and use that to
+    // index into items
+    let n = len(items);
+    for (let i = 0; i < n; i++) {
+      let el = refs[i];
+      if (el.contains(ev.target)) {
+        let item = items[i];
+        onclick(item);
+        return;
+      }
+    }
+    // console.log("didn't find item for ev.target:", ev.target);
+  }
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div
+  class="items overflow-y-auto cursor-pointer flex flex-row flex-wrap"
+  tabindex="-1"
+  role="listbox"
+  bind:this={listbox}
+  onclick={listboxclick}
+>
+  {#each items as item, idx (item.key)}
+    {@const isLast = idx == len(items) - 1}
+    <div
+      role="option"
+      aria-selected={idx === selectedIdx}
+      class=" py-0.5 px-2 leading-5 flex flex-row dark:text-opacity-50 hover:bg-gray-200 dark:hover:bg-gray-600 aria-selected:text-white aria-selected:bg-gray-400 dark:aria-selected:text-opacity-85 dark:aria-selected:bg-gray-700 whitespace-nowrap truncate max-w-[24ch]"
+      bind:this={refs[idx]}
+    >
+      {@render renderItem(item, idx)}
+    </div>
+    {#if !isLast}
+      <div class="mx-1 text-gray-400">&bull;</div>
+    {/if}
+  {/each}
+</div>
+
+<style>
+  :global(.os-scrollbar) {
+    --os-size: 10px;
+  }
+</style>
---
+++ src/components/NoteSelector2.svelte
@@ -0,0 +1,374 @@
+<script>
+  import {
+    getLatestNoteNames,
+    getMetadataForNote,
+    isSystemNoteName,
+    reassignNoteShortcut,
+    sanitizeNoteName,
+  } from "../notes";
+  import { getAltChar, isAltNumEvent, len } from "../util";
+  import { focus } from "../actions";
+  import ListBox from "./ListBox2.svelte";
+
+  /** @type {{
+    openNote: (name: string) => void,
+    createNote: (name: string) => void,
+    deleteNote: (name: string) => void,
+}}*/
+  let { openNote, createNote, deleteNote } = $props();
+
+  /**
+   * @typedef {Object} Item
+   * @property {string} name
+   * @property {string} nameLC
+   * @property {string} key
+   * @property {number} [altShortcut] - -1 if no shortcut, 0 to 9 for Alt-0 to Alt-9
+   * @property {HTMLElement} ref
+   */
+
+  /**
+   * @returns {Item[]}
+   */
+  function rebuildNotesInfo() {
+    const noteNames = getLatestNoteNames();
+    // console.log("rebuildNotesInfo, notes", noteInfos)
+    /** @type {Item[]} */
+    let res = Array(len(noteNames));
+    // let res = [];
+    for (let i = 0; i < len(noteNames); i++) {
+      let name = noteNames[i];
+      let item = {
+        key: name,
+        name: name,
+        nameLC: name.toLowerCase(),
+        ref: null,
+      };
+      let m = getMetadataForNote(item.name);
+      if (m && m.altShortcut) {
+        item.altShortcut = parseInt(m.altShortcut);
+      }
+      res[i] = item;
+    }
+    // -1 if a < b
+    // 0 if a = b
+    // 1 if a > b
+    res.sort((a, b) => {
+      // those with shortcut are before (<) those without
+      if (a.altShortcut && !b.altShortcut) {
+        return -1;
+      }
+      // those without shortcut are after (>) those with
+      if (!a.altShortcut && b.altShortcut) {
+        return 1;
+      }
+      // if both have shortcut, sort by shortcut
+      if (a.altShortcut && b.altShortcut) {
+        return a.altShortcut - b.altShortcut;
+      }
+      let isSysA = isSystemNoteName(a.name);
+      let isSysB = isSystemNoteName(b.name);
+      // system are last
+      if (isSysA && !isSysB) {
+        return 1;
+      }
+      if (!isSysA && isSysB) {
+        return -1;
+      }
+      // if both have no shortcut, sort by name
+      return a.name.localeCompare(b.name);
+    });
+    return res;
+  }
+  let itemsInitial = $state(rebuildNotesInfo());
+  let filter = $state("");
+  let altChar = $state(getAltChar());
+
+  let sanitizedFilter = $derived.by(() => {
+    return sanitizeNoteName(filter);
+  });
+
+  let filteredItems = $derived.by(() => {
+    // we split the search term by space, the name of the note
+    // must match all parts
+    let lc = sanitizedFilter.toLowerCase();
+    let parts = lc.split(" ");
+    let n = len(parts);
+    for (let i = 0; i < n; i++) {
+      let s = parts[i];
+      parts[i] = s.trim();
+    }
+    return itemsInitial.filter((noteInfo) => {
+      let s = noteInfo.nameLC;
+      for (let p of parts) {
+        if (s.indexOf(p) === -1) {
+          return false;
+        }
+      }
+      return true;
+    });
+  });
+
+  let selectedNote = $state(null);
+  let selectedName = $state("");
+  let canOpenSelected = $state(false);
+  let canCreate = $state(false);
+  let canCreateWithEnter = $state(false);
+  let canDeleteSelected = $state(false);
+  let showDelete = $state(false);
+
+  function selectionChanged(item, idx) {
+    // console.log("selectionChanged:", item, idx);
+    selectedNote = item;
+    selectedName = item ? selectedNote.name : "";
+    canOpenSelected = !!selectedNote;
+
+    // TODO: use lowerCase name?
+    let name = sanitizeNoteName(filter);
+    canCreate = len(name) > 0;
+    for (let i of filteredItems) {
+      if (i.name === name) {
+        canCreate = false;
+        break;
+      }
+    }
+
+    canCreateWithEnter = !canOpenSelected;
+    // if there are no matches for the filter, we can create with just Enter
+    // otherwise we need Ctrl + Enter
+    if (name.length === 0) {
+      canCreateWithEnter = false;
+    }
+
+    canDeleteSelected = false;
+    if (item && canOpenSelected) {
+      if (item.name !== "scratch" && !isSystemNoteName(item.name)) {
+        // can't delete scratch note or system notes
+        canDeleteSelected = true;
+      }
+    }
+
+    showDelete = canOpenSelected;
+  }
+
+  /**
+   * @param {string} name
+   * @returns {string}
+   */
+  function quoteNoteName(name) {
+    return `"` + sanitizeNoteName(name) + `"`;
+  }
+
+  /**
+   * @param {Item} note
+   * @returns {string}
+   */
+  function isSysNote(note) {
+    return isSystemNoteName(note.name) ? "italic" : "";
+  }
+
+  /**
+   * @param {KeyboardEvent} event
+   * @returns {boolean}
+   */
+  function isCtrlDelete(event) {
+    return (
+      (event.key === "Delete" || event.key === "Backspace") && event.ctrlKey
+    );
+  }
+
+  /**
+   * @param {Item} note
+   * @returns {string}
+   */
+  function noteShortcut(note) {
+    return note.altShortcut ? altChar + " + " + note.altShortcut : "";
+  }
+
+  /**
+   * @param {KeyboardEvent} event
+   */
+  function onKeydown(event) {
+    // console.log("onKeyDown:", event);
+    let altN = isAltNumEvent(event);
+    if (altN !== null) {
+      event.preventDefault();
+      let note = selectedNote;
+      if (note) {
+        reassignNoteShortcut(note.name, altN).then(() => {
+          itemsInitial = rebuildNotesInfo();
+        });
+        return;
+      }
+    }
+    let key = event.key;
+
+    if (key === "Enter") {
+      event.preventDefault();
+      let name = sanitizedFilter;
+      if (canCreateWithEnter) {
+        emitCreateNote(name);
+        return;
+      }
+      if (event.ctrlKey && canCreate) {
+        emitCreateNote(sanitizedFilter);
+        return;
+      }
+      if (selectedNote) {
+        emitOpenNote(selectedNote);
+      }
+    } else if (isCtrlDelete(event)) {
+      event.preventDefault();
+      if (!canDeleteSelected) {
+        return;
+      }
+      if (selectedNote) {
+        emitDeleteNote(selectedNote.name);
+      }
+      return;
+    }
+
+    if (key === "ArrowUp") {
+      event.preventDefault();
+      listbox.up();
+      return;
+    }
+
+    if (key === "ArrowLeft" && filter === "") {
+      event.preventDefault();
+      listbox.up();
+      return;
+    }
+
+    if (key === "ArrowDown") {
+      event.preventDefault();
+      listbox.down();
+      return;
+    }
+
+    if (key === "ArrowRight" && filter === "") {
+      event.preventDefault();
+      listbox.down();
+      return;
+    }
+  }
+
+  /**
+   * @param {Item} item
+   */
+  function emitOpenNote(item) {
+    // console.log("emitOpenNote", item);
+    openNote(item.name);
+  }
+
+  function emitCreateNote(name) {
+    // log("create note", name);
+    createNote(name);
+  }
+
+  /**
+   * @param {string} name
+   */
+  function emitDeleteNote(name) {
+    // console.log("deleteNote", name);
+    deleteNote(name);
+  }
+
+  let listbox;
+</script>
+
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<form
+  onkeydown={onKeydown}
+  tabindex="-1"
+  class="selector z-20 absolute center-x-with-translate top-[2rem] flex flex-col max-h-[90vh] w-[90vw] p-2"
+>
+  <input
+    type="text"
+    use:focus
+    bind:value={filter}
+    class="py-1 px-2 bg-white w-full mb-2 rounded-sm"
+  />
+  <ListBox
+    bind:this={listbox}
+    items={filteredItems}
+    {selectionChanged}
+    onclick={(item) => emitOpenNote(item)}
+  >
+    {#snippet renderItem(item)}
+      <div class="truncate {isSysNote(item) ? 'italic' : ''}">
+        {item.name}
+      </div>
+      {#if noteShortcut(item) !== ""}
+        <div class="ml-2 text-gray-600">({noteShortcut(item)})</div>
+      {/if}
+    {/snippet}
+  </ListBox>
+  {#if canOpenSelected || canDeleteSelected || filter.length > 0}
+    <hr class="mt-1 mb-1 border-gray-400" />
+  {/if}
+  <div
+    class="grid grid-cols-[auto_auto_1fr] gap-x-3 gap-y-3 mt-4 text-gray-700 text-size-[11px] leading-[1em] max-w-full dark:text-white dark:text-opacity-50"
+  >
+    {#if canOpenSelected}
+      <div><span class="kbd">Enter</span></div>
+      <div>open note</div>
+      <div class="font-bold truncate">
+        {quoteNoteName(selectedName)}
+      </div>
+    {/if}
+
+    {#if canCreateWithEnter}
+      <div><span class="kbd">Enter</span></div>
+    {/if}
+    {#if canCreate && !canCreateWithEnter}
+      <div>
+        <span class="kbd">Ctrl + Enter</span>
+      </div>
+    {/if}
+    {#if canCreate}
+      <div>create note</div>
+      <div class="font-bold truncate">
+        {quoteNoteName(filter)}
+      </div>
+    {/if}
+
+    {#if showDelete}
+      <div><span class="kbd">Ctrl + Delete</span></div>
+      <div class="red">delete note</div>
+    {/if}
+    {#if showDelete && canDeleteSelected}
+      <div class="font-bold truncate">
+        {quoteNoteName(selectedName)}
+      </div>
+    {/if}
+
+    {#if showDelete && !canDeleteSelected}
+      <div>
+        <span class="red"
+          >can't delete <span class="font-bold truncate"
+            >{quoteNoteName(selectedName)}</span
+          ></span
+        >
+      </div>
+    {/if}
+
+    <div><span class="kbd">{altChar} + 0...9</span></div>
+    <div class="col-span-2">assign quick access shortcut</div>
+
+    <div><span class="kbd">Esc</span></div>
+    <div>dismiss</div>
+    <div class="italic"></div>
+  </div>
+</form>
+
+<style>
+  .kbd {
+    font-size: 10px;
+    /* @apply text-xs; */
+    @apply font-mono;
+    @apply px-[6px] py-[3px];
+    @apply border  rounded-md;
+    @apply border-gray-400 dark:border-gray-500;
+    @apply bg-gray-50 dark:bg-gray-800;
+  }
+</style>
