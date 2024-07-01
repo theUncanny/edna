e3fdfa37dbb8a9d6b1b7ffe5898776943b1f9d6b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-07-01 21:53:21 +0200

    fix help page when Windows uses dark theme (fixes #8)

--- src/gen.mjs
+++ src/gen.mjs
@@ -166,7 +166,7 @@ let htmlStart = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline';" />
     <meta name="color-scheme" content="light dark">
-    <title>About Edna</title>
+    <title>Edna Help</title>
     <link rel="stylesheet" href="help.css">
   </head>
   <body>
